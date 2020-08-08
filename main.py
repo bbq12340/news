83e5889faf248eeaecef5b72370c5d228e16ca11
bbq12340 <bbq12340@hotmail.com>
2020-08-08 12:38:22 +0900

    last commit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import google
 import naver
 import pandas as pd
 import tkinter as tk
+from tkinter import messagebox
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime
+import csv
 
 root = tk.Tk()
 root.title("기사 크롤러")
@@ -13,23 +18,30 @@ recent = tk.IntVar()
 
 def start_crawl():
     print("Start Crawling")
+    CSVFILE = f'{keyword.get()}.csv'
     try:
         if recent.get() == 0:
             google_df = google.search_google(pages.get(), keyword.get())
             naver_df = naver.search_naver(pages.get(), keyword.get())
+            if len(google_df.index) == 0 or len(naver_df.index) == 0:
+                messagebox.showinfo("정보", "짧은 시간 내에 요청 수가 너무 많습니다.")
             frames = [google_df, naver_df]
             result = pd.concat(frames, ignore_index=True)
-            result.to_csv('sheet.csv')
-            print("finished")
+            result.to_csv(CSVFILE, encoding='utf-8')
+            messagebox.showinfo("정보", "크롤링 완료!")
         elif recent.get() == 1:
             google_df = google.search_google(pages.get(), keyword.get(), enable_recent=True)
             naver_df = naver.search_naver(pages.get(), keyword.get(), enable_recent=True)
             frames = [google_df, naver_df]
+            if len(google_df.index) == 0 or len(naver_df.index) == 0:
+                messagebox.showinfo("정보", "짧은 시간 내에 요청 수가 너무 많습니다.")
             result = pd.concat(frames, ignore_index=True)
-            result.to_csv('sheet.csv')
-            print("finished")
+            result.to_csv(CSVFILE, encoding='utf-8')
+            messagebox.showinfo("정보", "크롤링 완료!")
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        browser.get(f"https://www.google.com/search?q={keyword.get()}&source=lnms&tbm=fin")
     except IndexError:
-        print("구글 또는 네이버의 페이지를 넘어가는 요청의 수입니다.")
+        messagebox.showerror("에러", "구글 또는 네이버의 페이지를 넘어가는 요청의 수입니다.")
 #label
 keyword_label = tk.Label(root, text="키워드:")
 keyword_label.grid(row=0, column=0, padx=5, pady=5)
